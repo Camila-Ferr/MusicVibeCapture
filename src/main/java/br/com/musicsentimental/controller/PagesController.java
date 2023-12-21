bd5c854aca5083460ba87535d85a19bf478621a8
Camila Ferreira <cf_alves@id.uff.br>
2023-12-21 19:52:14 -0300

    Correção

--- src/main/java/br/com/musicsentimental/controller/PagesController.java
+++ src/main/java/br/com/musicsentimental/controller/PagesController.java
@@ -1,19 +1,39 @@
 package br.com.musicsentimental.controller;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class PagesController {
 	
 	@GetMapping("/")
-    public String index() {
-        return "index.html";
+    public ResponseEntity<String> index() throws IOException {
+		return getResource("static/index.html");
     }
 	
-	@GetMapping("/login")
-    public String login() {
-        return "login.html";
+    @GetMapping("/login")
+    public ResponseEntity<String> login() throws IOException {
+        return getResource("static/login.html");
     }
+	
+	private ResponseEntity<String> getResource(String path) throws IOException {
+        Resource resource = new ClassPathResource(path);
+        if (resource.exists()) {
+            byte[] content = Files.readAllBytes(Path.of(resource.getURI()));
+            return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(new String(content));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+	}
 
 }
